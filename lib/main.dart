1da94a8ba488e3a67885fe9d8338c676117a0ed6
tempTest <t2203530@partner.coway.com>
2022-03-31 13:22:06 +0900

    firebase 추가 (google-service / plist 제외)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() {
+  // await dotenv.load(fileName: ".env");
+
   runApp(const MyApp());
 }
 
